5860dd47114b6096788cb038ab241bb474bf0c12
Sohel Mallik <malliksohel582@gmail.com>
2026-08-08 19:58:19 +0530

    Download monaco editor

--- Frontend/src/App/App.jsx
+++ Frontend/src/App/App.jsx
@@ -1,12 +1,40 @@
 
+import "./app.css"
 
 function App() {
   
 
   return (
-    <>
-     
-    </>
+  
+      <main
+      className="h-screen w-full bg-gray-950 flex gap-4 p-4"
+    >
+      <aside
+        className="h-full w-1/4 bg-amber-50 rounded-lg "
+      >
+        <h2 className="text-2xl font-bold p-4 border-b border-gray-300">Users</h2>
+        <ul className="p-4">
+          {users.map((user, index) => (
+            <li key={index} className="p-2 bg-gray-800 text-white rounded mb-2">
+              {user.username}
+            </li>
+          ))}
+        </ul>
+
+      </aside>
+      <section
+        className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+          theme="vs-dark"
+          onMount={handleMount}
+        />
+      </section>
+
+    </main>
+    
   )
 }
 
